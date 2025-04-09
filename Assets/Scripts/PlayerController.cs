b32d134b69f2305e3fe2e91998edf75d8eb5150e
General-Monish <monissam28@gmail.com>
2025-04-10 02:02:38 +0530

    Crouch and Jump Animation Handled

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,40 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    /*[SerializeField] float speed;*/
-
+    /*[SerializeField] float speed = 5f;*/
     [SerializeField] Animator animator;
-    
-    // Start is called before the first frame update
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal"); // for left right input
-        animator.SetFloat("Speed", Mathf.Abs(horizontalInput)); // mathf for no negative values
+        HandleMovement();
+        HandleCrouch();
+        HandleJump();
+    }
+
+    void HandleMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
 
         Vector3 scale = transform.localScale;
 
         if (horizontalInput < 0)
         {
-            scale.x = -Mathf.Abs(scale.x); // Keep it -2 if it was 2
+            scale.x = -Mathf.Abs(scale.x);
         }
         else if (horizontalInput > 0)
         {
-            scale.x = Mathf.Abs(scale.x); // Keep it +2
+            scale.x = Mathf.Abs(scale.x);
         }
 
         transform.localScale = scale;
 
     }
 
+    void HandleCrouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", true);
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+        }
+    }
 
+    void HandleJump()
+    {
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+    }
 }
