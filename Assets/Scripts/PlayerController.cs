3171140c019dae5a1c146b779f4ce98f97aa3f61
General-Monish <monissam28@gmail.com>
2025-04-10 20:42:07 +0530

    Added movement using horizontal input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    /*[SerializeField] float speed = 5f;*/
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float groundCheckDistance = 2f;
+    [SerializeField] LayerMask groundLayer;
+
     [SerializeField] Animator animator;
 
     private Rigidbody2D rb;
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        HandleMovement();
-        HandleCrouch();
-        HandleJump();
-    }
-
-    void HandleMovement()
-    {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
+        HandleMovement(horizontalInput);
+        PlayerMovementAnimationLeftAndRight(horizontalInput);
+        HandleCrouch();
+        Jump();
+    }
+    
+    void HandleMovement( float horizontal)
+    {
+        Vector3 MovementDirection = transform.position;
+        MovementDirection.x += horizontal * speed * Time.deltaTime;
+        transform.position= MovementDirection;
+    }
+    private void PlayerMovementAnimationLeftAndRight(float horizontalInput)
+    {
         animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
 
         Vector3 scale = transform.localScale;
@@ -36,11 +47,8 @@ public class PlayerController : MonoBehaviour
         {
             scale.x = Mathf.Abs(scale.x);
         }
-
         transform.localScale = scale;
-
     }
-
     void HandleCrouch()
     {
         if (Input.GetKey(KeyCode.LeftControl))
@@ -52,16 +60,32 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("Crouch", false);
         }
     }
-
-    void HandleJump()
+    void Jump()
     {
-        if (Input.GetAxisRaw("Vertical") > 0)
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && OnGround())
         {
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
             animator.SetBool("Jump", true);
         }
-        else
+        else if (OnGround())
         {
             animator.SetBool("Jump", false);
         }
     }
+    private bool OnGround()
+    {
+        Vector2 pos = transform.position;
+        Vector2 dir = Vector2.down;
+        float distance = groundCheckDistance;
+        RaycastHit2D raycastHit2DHit = Physics2D.Raycast(pos, dir, groundCheckDistance,groundLayer);
+        if (raycastHit2DHit.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
